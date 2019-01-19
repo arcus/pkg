1c0945ccb59b5138f0bae291499a14be6b2f36e6
Byron Ruth <b@devel.io>
2019-01-18 21:20:01 -0500

    Change transport/http.NewServer to return an *echo.Echo (#11)
    The perceived added value of a custom Service was to compose service or
    HTTP middleware like Echo does and to support nested groups and such.
    However, it adds a lot of complexity and code to maintain when service
    middleware composition can be done via a single function. Echo already
    handles the HTTP middleware composition and grouping mechanisms.
    
    A related change is to simplify `MakeDefaultAdaptor` to just
    `DefaultAdaptor` which takes the handler and a pointer value of the
    concrete proto.Message type modeling the request.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- transport/http/adaptor.go
+++ transport/http/adaptor.go
@@ -14,48 +14,46 @@ import (
 
 // Adaptor is a function that converts a service.Handler for a request type
 // into an echo.HandlerFunc.
-type Adaptor func(next service.Handler) echo.HandlerFunc
+type Adaptor func(hdlr service.Handler) echo.HandlerFunc
 
-// MakeDefaultAdaptor returns an Adaptor that decodes the request into the
+// DefaultAdaptor returns an Adaptor that decodes the request into the
 // proto.Message request type and passes it to the handler, returning any error
 // that results, an HTTP OK with no body if the response is empty, or the JSON
 // encoded response as the body of an HTTP OK response.
-func MakeDefaultAdaptor(req proto.Message) Adaptor {
-	return func(next service.Handler) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			ctx := ContextFromRequest(c.Request())
+func DefaultAdaptor(hdlr service.Handler, req proto.Message) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := ContextFromRequest(c.Request())
 
-			// Make a copy.
-			req = copyProto(req)
+		// Make a copy.
+		req = copyProto(req)
 
-			// Decode the request body into the request type.
-			// Note: this assumes that the custom protoBinder has been
-			// registered with the echo instance.
-			if err := c.Bind(req); err != nil {
-				return err
-			}
-
-			rep, err := next(ctx, req)
-			if err != nil {
-				return err
-			}
-
-			// Write an empty response.
-			if _, ok := rep.(*emptypb.Empty); ok || rep == nil {
-				return c.NoContent(http.StatusOK)
-			}
-
-			// Write the response as a JSON-encoded value.
-			// TODO: this is wasting buffers..
-			var buf bytes.Buffer
-			m := &jsonpb.Marshaler{
-				OrigName: true,
-			}
-			if err := m.Marshal(&buf, rep); err != nil {
-				return err
-			}
-			return c.JSONBlob(http.StatusOK, buf.Bytes())
+		// Decode the request body into the request type.
+		// Note: this assumes that the custom protoBinder has been
+		// registered with the echo instance.
+		if err := c.Bind(req); err != nil {
+			return err
 		}
+
+		rep, err := hdlr(ctx, req)
+		if err != nil {
+			return err
+		}
+
+		// Write an empty response.
+		if _, ok := rep.(*emptypb.Empty); ok || rep == nil {
+			return c.NoContent(http.StatusOK)
+		}
+
+		// Write the response as a JSON-encoded value.
+		// TODO: this is wasting buffers..
+		var buf bytes.Buffer
+		m := &jsonpb.Marshaler{
+			OrigName: true,
+		}
+		if err := m.Marshal(&buf, rep); err != nil {
+			return err
+		}
+		return c.JSONBlob(http.StatusOK, buf.Bytes())
 	}
 }
 
--- transport/http/server.go
+++ transport/http/server.go
@@ -1,133 +1,10 @@
 package http
 
 import (
-	"context"
-	"net/http"
-	"net/url"
-
-	"github.com/arcus/pkg/service"
 	"github.com/arcus/pkg/status"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
-
-// NewServer returns a new server that can bind protobuf messages, converts
-// errors to `arcus.pkg.status.Status`s, logs requests as JSON, and converts
-// request contexts to the custom `arcus.pkg.arcus.Context`.
-func NewServer() *Server {
-	e := echo.New()
-	e.HideBanner = true
-	e.HidePort = true
-	e.Binder = &ProtoBinder{}
-	e.HTTPErrorHandler = NewErrorHandler(e)
-	e.Use(
-		middleware.Logger(),
-		ServiceContextMiddleware,
-	)
-	return &Server{echo: e}
-}
-
-// Server serves traffic using the routes defined on it.
-type Server struct {
-	httpMiddleware []echo.MiddlewareFunc
-	middleware     []service.Middleware
-
-	echo *echo.Echo
-}
-
-// Use wraps the handler in middleware, which will be applied last first.
-func (s *Server) Use(m ...service.Middleware) {
-	s.middleware = append(s.middleware, m...)
-}
-
-// UseForHTTP uses middleware at the transport layer, applied last first.
-func (s *Server) UseForHTTP(m ...echo.MiddlewareFunc) {
-	s.httpMiddleware = append(s.httpMiddleware, m...)
-}
-
-// Query creates an endpoint for query operations at path that
-// handles the request type with the handler wrapped in middleware.
-func (s *Server) Query(path string, a Adaptor, h service.Handler, m ...service.Middleware) *Route {
-	return s.add(http.MethodGet, path, a, h, m...)
-}
-
-// Command creates an endpoint for command operations at path that
-// handles the request type with the handler wrapped in middleware.
-func (s *Server) Command(path string, a Adaptor, h service.Handler, m ...service.Middleware) *Route {
-	return s.add(http.MethodPost, path, a, h, m...)
-}
-
-// Group creates a group at path using middleware.
-func (s *Server) Group(path string, m ...service.Middleware) *Group {
-	nm := make([]service.Middleware, 0, len(s.middleware)+len(m))
-	nm = append(nm, s.middleware...)
-	nm = append(nm, m...)
-	return &Group{
-		prefix:         path,
-		httpMiddleware: s.httpMiddleware,
-		middleware:     nm,
-		echo:           s.echo,
-	}
-}
-
-// Proxy proxies all requests with path prefix to uris, removing
-// path before relay.
-func (s *Server) Proxy(path string, uris ...string) error {
-	var ts []*middleware.ProxyTarget
-
-	for _, uri := range uris {
-		u, err := url.Parse(uri)
-		if err != nil {
-			return err
-		}
-		ts = append(ts, &middleware.ProxyTarget{URL: u})
-	}
-
-	m := append(s.httpMiddleware, middleware.ProxyWithConfig(
-		middleware.ProxyConfig{
-			Balancer: middleware.NewRoundRobinBalancer(ts),
-			Rewrite:  map[string]string{path + "/*": "/$1"},
-		},
-	))
-
-	s.echo.Group(path, m...)
-	return nil
-}
-
-// ServeHTTP implements `http.Handler` interface, which serves HTTP requests.
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	s.echo.ServeHTTP(w, r)
-}
-
-// Start starts the server, listening at addr.
-func (s *Server) Start(addr string) error {
-	return s.echo.Start(addr)
-}
-
-// Shutdowns shuts down the server.
-func (s *Server) Shutdown(ctx context.Context) error {
-	return s.echo.Shutdown(ctx)
-}
-
-func (s *Server) add(method, path string, a Adaptor, h service.Handler, m ...service.Middleware) *Route {
-	// Combine into a new slice to avoid accidentally passing the same slice for
-	// multiple routes, which would lead to later add() calls overwriting the
-	// middleware from earlier calls.
-	nm := make([]service.Middleware, 0, len(s.middleware)+len(m))
-	nm = append(nm, s.middleware...)
-	nm = append(nm, m...)
-	r := &Route{
-		adaptor:        a,
-		method:         method,
-		path:           path,
-		handler:        h,
-		httpMiddleware: s.httpMiddleware,
-		middleware:     nm,
-		echo:           s.echo,
-	}
-	r.build()
-	return r
-}
 
 func NewErrorHandler(mux *echo.Echo) echo.HTTPErrorHandler {
 	return func(err error, c echo.Context) {
@@ -145,4 +22,20 @@ func ServiceContextMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 		c.SetRequest(c.Request().WithContext(ctx))
 		return next(c)
 	}
+}
+
+// NewServer returns a new Echo server that can bind protobuf messages, converts
+// errors to `arcus.pkg.status.Status`s, logs requests as JSON, and converts
+// request contexts to the custom `arcus.pkg.service.Context`.
+func NewServer() *echo.Echo {
+	e := echo.New()
+	e.HideBanner = true
+	e.HidePort = true
+	e.Binder = &ProtoBinder{}
+	e.HTTPErrorHandler = NewErrorHandler(e)
+	e.Use(
+		middleware.Logger(),
+		ServiceContextMiddleware,
+	)
+	return e
 }
