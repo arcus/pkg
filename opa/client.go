c1f6187925c07cc9ee9cd4447790fa9ad261bddf
Byron Ruth <b@devel.io>
2019-05-01 07:51:26 -0400

    Add Authorized method to HTTP client
    Signed-off-by: Byron Ruth <b@devel.io>

--- opa/client.go
+++ opa/client.go
@@ -8,6 +8,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"github.com/arcus/pkg/service"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+)
+
+const (
+	ErrNoDecision = Error("opa: no decision")
 )
 
 type Error string
@@ -16,16 +24,68 @@ func (e Error) Error() string {
 	return string(e)
 }
 
-const (
-	ErrNoDecision = Error("opa: no decision")
-)
+type jsonResult struct {
+	Result interface{}
+}
+
+type authzResult struct {
+	Allow  bool
+	Claims interface{}
+}
+
+// newServiceInput derives an authorization request input from the service context and request itself.
+func newServiceInput(ctx *service.Context, req proto.Message) (interface{}, error) {
+	mpb := &jsonpb.Marshaler{
+		OrigName: true,
+	}
+
+	reqjs, err := mpb.MarshalToString(req)
+	if err != nil {
+		return nil, err
+	}
+
+	input := map[string]interface{}{
+		"service": map[string]interface{}{
+			"name":    ctx.ServiceName,
+			"version": ctx.ServiceVersion,
+		},
+		"principal": map[string]interface{}{
+			"id":   ctx.PrincipalId,
+			"role": ctx.PrincipalRole,
+		},
+		"operation": map[string]interface{}{
+			"id":    proto.MessageName(req),
+			"input": json.RawMessage(reqjs),
+		},
+	}
+
+	return input, nil
+}
 
 type Client struct {
 	Addr string
 	HTTP *http.Client
 }
 
-func (c *Client) Query(ctx context.Context, policy string, input, result interface{}) error {
+func (c *Client) Authorized(ctx *service.Context, policy string, req proto.Message, claims interface{}) (bool, error) {
+	input, err := newServiceInput(ctx, req)
+	if err != nil {
+		return false, err
+	}
+
+	result := authzResult{
+		Claims: claims,
+	}
+
+	err = c.Query(ctx, policy, input, &result)
+	if err != nil {
+		return false, err
+	}
+
+	return result.Allow, nil
+}
+
+func (c *Client) Query(ctx context.Context, policy string, input interface{}, result interface{}) error {
 	path := strings.Replace(strings.Replace(policy, ".", "/", -1), "-", "_", -1)
 	url := fmt.Sprintf("%s/v1/data/%s", c.Addr, path)
 
@@ -61,10 +121,6 @@ func (c *Client) Query(ctx context.Context, policy string, input, result interfa
 	return json.Unmarshal(b, &jsonResult{
 		Result: result,
 	})
-}
-
-type jsonResult struct {
-	Result interface{}
 }
 
 func NewClient(addr string) *Client {
